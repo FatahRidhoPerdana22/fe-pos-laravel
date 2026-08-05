56b9f0562c797f438912fc7bdbc0c427f37d0854
FatahRidhoPerdana22 <fatahridho27@gmail.com>
2026-08-06 00:30:54 +0700

    .

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,50 +1,51 @@
-import axios from 'axios';
-import { useLoadingStore } from '@/stores/loading';
+import axios from "axios";
+import { useLoadingStore } from "@/stores/loading";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        Accept: 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Accept: "application/json",
+  },
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    },
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
 );
 
 api.interceptors.request.use(
-    (config) => {
-        const loadingStore = useLoadingStore();
-        loadingStore.show();
-        return config;
-    },
-    (error) => {
-        const loadingStore = useLoadingStore();
-        loadingStore.hide();
-        return Promise.reject(error);
-    },
+  (config) => {
+    const loadingStore = useLoadingStore();
+    loadingStore.show();
+    return config;
+  },
+  (error) => {
+    const loadingStore = useLoadingStore();
+    loadingStore.hide();
+    return Promise.reject(error);
+  },
 );
 
 api.interceptors.response.use(
-    (response) => {
-        const loadingStore = useLoadingStore();
-        loadingStore.hide();
-        return response;
-    },
-    (error) => {
-        const loadingStore = useLoadingStore();
-        loadingStore.hide();
-        return Promise.reject(error);
-    },
+  (response) => {
+    const loadingStore = useLoadingStore();
+    loadingStore.hide();
+    return response;
+  },
+  (error) => {
+    const loadingStore = useLoadingStore();
+    loadingStore.hide();
+    return Promise.reject(error);
+  },
 );
 
 export default api;
